1ca312c86a9b6ebf8256cdc56ff12d39e4fe482e
Sydney_Hunter <sydney.jane.hunter@gmail.com>
2020-03-04 20:14:39 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Sydney"
+  end
+
+  get '/hometown' do
+    "My hometown is Albuquerque"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Oh My Dear Lord"
+  end
 end
